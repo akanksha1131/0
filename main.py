9528cac80562c7bc6c6c605c4629d4ee4da33287
akanksha <akanksha.wrk@gmail.com>
2024-02-08 17:42:52 +0530

    sentiment analysis of a newpaper article

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import nltk
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.tag import pos_tag
-text = "Time to get started with natural language processing!"
-word_tokens = word_tokenize(text)
-tags = pos_tag(word_tokens)
-print(tags)
+from textblob import TextBlob
+from newspaper import Article
+url = 'https://www.hindustantimes.com/entertainment/bollywood/poonam-pandey-fake-death-celebs-react-101706945987655.html'
+article = Article(url)
+article.download()
+article.parse()
+article.nlp()
+text = article.text
+text = article.summary
+print(text)
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity
+print(sentiment)
