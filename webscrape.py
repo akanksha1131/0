7acab44d4ff8b4f3d32fd81cf60c470a133443c1
Akanksha Kale <akanksha.wrk@gmail.com>
2024-03-27 20:27:25 +0530

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 URL='https://www.bbc.com/news'
 response=requests.get(URL)
 print("The response code is: ", response)
-
 #parse the html document
 soup=BeautifulSoup(response.content, 'html.parser')
 print(soup)
